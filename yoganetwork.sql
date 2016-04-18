91c86aea257afa0346f1aee6091bd62b595456d0
Pauliina Solanne <pauliinasolanne@Pauliina-Air.lan>
2016-04-18 22:11:01 +1000

    files

---
+++ yoganetwork.sql
@@ -0,0 +1,40 @@
+CREATE DATABASE yoganetwork;
+
+\c yoganetwork
+
+CREATE TABLE studios (
+  id SERIAL4 PRIMARY KEY,
+  name VARCHAR (200),
+  image_url VARCHAR (500),
+  suburb VARCHAR (100)
+  style VARCHAR (1000)
+);
+
+SELECT * FROM studios;
+
+INSERT INTO studios (name, image_url, suburb, style) VALUES ('Power Living', 'http://tinanded.com.au/wp-content/uploads/2013/12/tinanded-power-living-1-1024x682.jpg', 'Fitzroy, South Melbourne', '');
+
+CREATE TABLE yogis (
+  id SERIAL4 PRIMARY KEY,
+  name VARCHAR (200),
+  image_url VARCHAR (500),
+  suburb VARCHAR (100)
+  favourite_yoga VARCHAR (500)
+);
+
+
+CREATE TABLE yoga_styles (
+  id SERIAL4 PRIMARY KEY,
+  yoga VARCHAR (200),
+  level VARCHAR (100)
+  mood VARCHAR (500)
+);
+
+
+CREATE TABLE admins (
+  id SERIAL4 PRIMARY KEY,
+  name VARCHAR (200),
+  image_url VARCHAR (500),
+  suburb VARCHAR (100)
+  favourite_yoga VARCHAR (500)
+);
