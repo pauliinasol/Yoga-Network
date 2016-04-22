506665392e1d667df16966787c307c7dc649cfd4
Pauliina Solanne <pauliinasolanne@Pauliina-MacBook-Air.local>
2016-04-22 10:26:10 +1000

    fixed

--- public/javascripts/cbpAnimatedHeader.js
+++ public/javascripts/cbpAnimatedHeader.js
@@ -4,7 +4,7 @@
  *
  * Licensed under the MIT license.
  * http://www.opensource.org/licenses/mit-license.php
- * 
+ *
  * Copyright 2013, Codrops
  * http://www.codrops.com
  */
@@ -27,10 +27,10 @@ var cbpAnimatedHeader = (function() {
 	function scrollPage() {
 		var sy = scrollY();
 		if ( sy >= changeHeaderOn ) {
-			classie.add( header, 'navbar-shrink' );
+			// classie.add( header, 'navbar-shrink' );
 		}
 		else {
-			classie.remove( header, 'navbar-shrink' );
+			// classie.remove( header, 'navbar-shrink' );
 		}
 		didScroll = false;
 	}
