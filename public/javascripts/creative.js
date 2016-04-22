59b8f10074ee73b8451d79041a0414ee365205e8
Pauliina Solanne <pauliinasolanne@Pauliina-MacBook-Air.local>
2016-04-22 11:58:26 +1000

    final

--- public/javascripts/creative.js
+++ public/javascripts/creative.js
@@ -16,11 +16,7 @@
         event.preventDefault();
     });
 
-    // Highlight the top nav as scrolling occurs
-    $('body').scrollspy({
-        target: '.navbar-fixed-top',
-        offset: 51
-    })
+
 
     // Closes the Responsive Menu on Menu Item Click
     $('.navbar-collapse ul li a').click(function() {
@@ -28,19 +24,10 @@
     });
 
     // Fit Text Plugin for Main Header
-    $("h1").fitText(
-        1.2, {
-            minFontSize: '35px',
-            maxFontSize: '65px'
-        }
-    );
+
 
     // Offset for Main Navigation
-    $('#mainNav').affix({
-        offset: {
-            top: 100
-        }
-    })
+
 
     // Initialize WOW.js Scrolling Animations
     new WOW().init();
